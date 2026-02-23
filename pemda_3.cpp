02349f0fd7967534c8d4461a949a8897b808defa
Semutmanis <aldidermawan1@gmail.com>
2026-02-23 16:16:24 +0700

    commit 2 (menhitung persegi panjang)

---
+++ pemda_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "menghitung luas persegi panjang" << endl;
+
+  int p, l;
+  float luas;
+
+  cout << "masukan panjang = ";
+  cin >> p;
+
+  cout << "masukan lebar =  ";
+  cin >> l;
+
+  luas = p*l;
+
+  cout << "hasilnya = " << luas;
+
+
+
+}

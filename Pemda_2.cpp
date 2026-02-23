d170f99ffca244dd85a9fa34a2ff747777888ab2
Semutmanis <aldidermawan1@gmail.com>
2026-02-23 15:46:03 +0700

    commit pertama

---
+++ Pemda_2.cpp
@@ -0,0 +1,18 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ // program utama
+    int age; // variabel local
+    int age1; // variabel local
+    age = 15;
+    age1 = 20;
+
+    cout << "hallo world" << endl;
+    cout << 99 << endl;
+    cout << age << endl;
+    cout << age1 << endl;
+}
